8676c7e251ac076052b49b686b39839ab8203057
mgrzwcz <55543943+mgrzwcz@users.noreply.github.com>
2020-02-07 17:31:07 +0000

    Added AccountClassExercise

---
+++ AccountClassExercise/Exercise 5 - Account/Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountClassExercise
+{
+    class Account
+    {
+        public decimal Balance { get; set; }
+
+        public Account(decimal InitialBalance)
+        {
+            if (Balance == 0)
+            {
+                Balance = InitialBalance;
+            }
+            
+        }
+
+        public bool Credit(decimal amount)
+        {
+            
+            if((Balance + amount) > Balance){
+                Balance += amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Debit(decimal amount)
+        {
+
+            if ((Balance - amount) < Balance && (Balance - amount) >= 0)
+            {
+                Balance -=  amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Account balance: {Balance}{Environment.NewLine}Crediting 200 successful? {Credit(200)}{Environment.NewLine}"
+                + $"Debiting 500 successful? {Debit(500)}{Environment.NewLine}Final balance: {Balance}{Environment.NewLine}";
+        }
+
+
+
+
+
+    }
+}
+
+
---
+++ AccountClassExercise/Exercise 5 - Account/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountClassExercise
+{
+
+    /*
+     * Write an Account class in C# with the following specification:
+        A constructor with one parameter to initialise the balance with an opening amount.
+        A method to credit the account balance by specified amount with the following header public
+        bool Credit( decimal amount )
+        A method to debit the account balance by a specified amount with the following header.  The method should return bool indicating whether the debit was successful.
+        public bool Debit( decimal amount )
+        Also write an auto-implemented property that gets and sets the account balance.
+        Write a test code in your Main() function and create two account objects and test all methods. 
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account test1 = new Account(10000);
+
+            Console.WriteLine(test1);
+
+            Account test2 = new Account(100);
+
+            Console.WriteLine(test2);
+        }
+    }
+}
